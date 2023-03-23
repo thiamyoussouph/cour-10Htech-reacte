d81397c45ab03d97fe9f3ebf5125042899909a16
youssouph <thiamasjo@gmail.com>
2023-03-23 16:33:12 +0000

    fist commit

---
+++ src/Compemens/Sidebard.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Sidebard(props) {
+    return (
+        <>
+            <div >
+                <h1 className='text-grey-50'>Sidebard</h1>
+
+            </div>
+
+        </>
+    );
+}
+
+export default Sidebard;
